b1cd230edead65476dd12e8157786fd44788d8c9
sethajoh <seth.adams.johnson@gmail.com>
2015-11-29 07:10:51 -0800

    minor model changes, added minimal server

---
+++ server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PATH="/home/serth/perl5/perlbrew/bin:/home/serth/perl5/perlbrew/perls/perl-5.20.3/bin:/home/serth/.rbenv/bin:/home/serth/usr/local/bin:/home/serth/.rbenv/shims:/home/serth/.rbenv/bin:/home/serth/.gems/bin:/usr/local/bin:/usr/bin:/bin"
+PERLBREW_PATH="/home/serth/perl5/perlbrew/perls/perl-5.20.3/bin"
+HYPNOTOAD="/home/serth/perl5/perlbrew/perls/perl-5.20.3/bin/hypnotoad"
+$HYPNOTOAD /home/serth/tharn-mojo/tharn.pl
+
--- tharn.pl
+++ tharn.pl
@@ -2,39 +2,28 @@
 use Mojolicious::Lite;
 use DBI;
 use File::Pairtree;
+use Mojo::UserAgent;
 
-state $dbh = DBI->connect("dbi:SQLite:dbname=tharn.sqlite.2015-10-20.db","","");
+state $dbh = DBI->connect("dbi:SQLite:dbname=tharn.db","","");
 
 my $static = app->static();
 push @{ $static->paths }, "/home/serth/tharn.org/public/";
 
-#plugin Config => { file => 'tharn.conf' };
 plugin 'Config';
 
 plugin 'AssetPack';
 
 my @js_files = (
     "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.js",
     "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular.js",
     "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular-route.js",
     "https://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.2.0/fabric.all.min.js",
     "js/tharn.js",
-    "js/bigpicture.js"
 );
 
 app->asset(
     'app.js' => @js_files
 );
-
-app->asset(
-    'style.css' => (
-        "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap-theme.min.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css",
-    )
-);
-
 
 
 get '/' => sub {
@@ -45,21 +34,48 @@ get '/' => sub {
 
 get '/bigpicture' => sub {
     my $self = shift;
-    my $word = $dbh->selectrow_hashref("select * from word order by RANDOM() limit 1");
 
-    my $sth = $dbh->prepare("select file from image where word_id = ?");
-    $sth->execute($word->{'id'});
+    my $sth = $dbh->prepare("select id, word from word where images >= 10 order by random() limit 1");
+    $sth->execute;
+    my ($id, $word) = $sth->fetchrow;
+
+    $sth = $dbh->prepare("select file from image where word_id = ?");
+    $sth->execute($id);
+    my $images = $sth->fetchall_arrayref;
+
 
     my @images;
-    while (my $file = $sth->fetchrow) {
-        $file =~ s/^\/home\/serth\/tharn\.org\/public//;
-        push @images, $file;
+    for my $file (@$images) {
+        $file->[0] =~ s/^\/home\/serth\/tharn\.org\/public//;
+        push @images, $file->[0];
     }
 
     $self->render(json => {
-        word => $word->{'word'},
-        images => \@images
+        word    => $word,
+        images  => \@images
     }, status => 200);
+};
+
+
+get '/fetch' => sub {
+    my $self = shift;
+
+    my $sth = $dbh->prepare("select word from word where images = 0 order by random() limit 1");
+    $sth->execute;
+    my $word = $sth->fetchrow;
+   
+    my $azure = plugin 'Config' => { file => 'azure.conf' };
+    my $url = 'https://user:'
+        . $azure->key
+        . '@api.datamarket.azure.com/Data.ashx/Bing/Search/v1/Image?Query=%27'
+        . $word
+        .'%27&$top=20&$format=JSON';
+
+    my $ua = Mojo::UserAgent->new;
+    my $res = $ua->get($url)->res->json;
+    
+    $self->app->log->debug($res);
+    
 };
 
 
@@ -72,7 +88,6 @@ __DATA__
 <html>
     <head>
         <title><%= title %></title>
-        <%= asset 'style.css' %>
     </head>
     <body ng-app="Tharn">
 
