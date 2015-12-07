ddfedbfad38e453cbdb61c61cea165feff102b44
sethajoh <seth.adams.johnson@gmail.com>
2015-12-07 11:41:43 -0800

    working on fetch/

--- tharn.pl
+++ tharn.pl
@@ -1,8 +1,12 @@
 #!/usr/bin/env perl;
 use Mojolicious::Lite;
 use DBI;
-use File::Pairtree;
+use File::Pairtree qw/id2ppath/;
 use Mojo::UserAgent;
+use File::Path qw/make_path/;
+use Mojo::URL;
+use Mojolicious::Types;
+use File::Basename;
 
 state $dbh = DBI->connect("dbi:SQLite:dbname=tharn.db","","");
 
@@ -60,11 +64,12 @@ get '/bigpicture' => sub {
 get '/fetch' => sub {
     my $self = shift;
 
-    my $sth = $dbh->prepare("select word from word where images = 0 order by random() limit 1");
+    my $sth = $dbh->prepare("select id, word from word where images = 0 order by random() limit 1");
     $sth->execute;
-    my $word = $sth->fetchrow;
+    my ($word_id, $word) = $sth->fetchrow;
    
     my $azure = plugin 'Config' => { file => 'azure.conf' };
+
     my $url = 'https://user:'
         . $azure->{'key'}
         . '@api.datamarket.azure.com/Data.ashx/Bing/Search/v1/Image?Query=%27'
@@ -72,24 +77,55 @@ get '/fetch' => sub {
         .'%27&$top=20&$format=JSON';
 
     my $ua = Mojo::UserAgent->new;
-    my $res = $ua->get($url => {Accept => '*/*'})->res->json;
+    my $res = $ua->get($url => {Accept => '*/*'})->res;
+    my $json = $res->json;
 
-    my @images;
-    my @thumbs;
-    for my $r (@{ $res->{'d'}->{'results'} }) {
+    my $pairtree = id2ppath($word);
+    $pairtree =~ s/^pairtree_root\///;
 
-        my $image = $r->{'MediaUrl'};
-        my $thumb = $r->{'Thumbnail'}->{'MediaUrl'};
+    my $path = "/home/serth/tharn.org/public/words/$pairtree" . "$word";
+    make_path($path);
+    $res->content->asset->move_to($path . "/$word.json");
 
-        push @images, $image;
-        push @thumbs, $thumb;
+    make_path($path . "/thumbs/");
+    make_path($path . "/images/");
+
+    # This is blocking :P
+    for my $r (@{ $json->{'d'}->{'results'} }) {
+
+        my $im_url = $r->{'MediaUrl'};
+        my $th_url = $r->{'Thumbnail'}->{'MediaUrl'};
+
+        my $id = Mojo::URL->new($th_url)->query->param('id');
+
+        # Azure is returning 'image/jpg' not 'image/jpeg', register it
+        my $types = Mojolicious::Types->new;
+        $types->mapping({ jpg => ['image/jpg'] });
+        my $exts = $types->detect($r->{'Thumbnail'}->{'ContentType'});
+    
+        # Get thumbnail
+        my $thumb = $path . "/thumbs/" . $id . "." . $exts->[0];
+        $ua->get($th_url)->res->content->asset->move_to($thumb);
+
+        # Get image
+        my $image = $path . "/images/" . $id . "--". basename(Mojo::URL->new($im_url)->path);
+        $ua->get($im_url)->res->content->asset->move_to($image);
+
+        # Update db
+        if (-e $thumb) {
+            $dbh->do("insert into thumb (word_id, file) values (?, ?)", undef, $word_id, $thumb);  
+            $self->app->log->info("Added $word, $thumb");
+        }
+        if (-e $image) {
+            $dbh->do("insert into image (word_id, file) values (?, ?)", undef, $word_id, $thumb);
+            $self->app->log->info("Added $word, $image");
+        }
+
     }
+
+
+    $self->rendered(200); 
     
-    
-    $self->render(json => { 
-        images => \@images,
-        thumbs => \@thumbs 
-    }, status => 200);
 };
 
 
